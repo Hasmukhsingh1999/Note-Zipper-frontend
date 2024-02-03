e9c79e8670ad8e6a2971fbeae6d7285f5d2e600f
Hasmukhsingh1999 <hasmukhsingh90@gmail.com>
2024-02-03 20:33:59 +0530

    restructure

--- src/app/sign-up/page.js
+++ src/app/sign-up/page.js
@@ -2,12 +2,18 @@
 import { useCreateUserMutation } from "@/api/auth-api";
 import AuthPage from "@/components/Auth/AuthPage";
 import axios from "axios";
-import React from "react";
-
-
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import { Card, Input, Space, message } from "antd";
+import {
+  UserOutlined,
+  EyeInvisibleOutlined,
+  EyeTwoTone,
+} from "@ant-design/icons";
+import Link from "next/link";
 const page = () => {
-  const [createUser,{data}] = useCreateUserMutation()
-  const handleSignUp = async ({ username, email, password }) => {
+  const [createUser, { data }] = useCreateUserMutation();
+  const handleSignUp = async ({ name, email, password }) => {
     try {
       // Make the API call to create a new user
       // const result = await createUser({
@@ -18,9 +24,9 @@ const page = () => {
 
       const axiosResult = await axios.post("http://localhost:8000/api/users", {
         // Add the necessary data or parameters for your request
-        username,
+        name,
         email,
-        password
+        password,
       });
 
       // Handle the result of the Axios request
@@ -33,14 +39,96 @@ const page = () => {
     }
   };
 
+  const [passwordVisible, setPasswordVisible] = useState(false);
+
+  const backgroundImageUrl =
+    "https://images.unsplash.com/photo-1584448033614-882b971a36f3?q=80&w=1958&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+
+  const cardStyle = {
+    textAlign: "center",
+    boxShadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px",
+    backgroundColor: "#fcf8f3",
+  };
+
+  const imageStyle = {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+    objectFit: "cover",
+  };
+
   return (
-    <div>
-      <AuthPage
-        title="Sign Up"
-        buttonLabel="Sign Up"
-        onSubmit={handleSignUp}
-        isRegistered={"Sign-up"}
-      />
+    <div className="w-full flex">
+      <div className="flex-auto md:w-32 w-full h-screen md:flex hidden">
+        <div className="w-full h-full">
+          <img
+            src={backgroundImageUrl}
+            className="w-full h-full object-cover"
+            alt="Background"
+          />
+        </div>
+      </div>
+      <div className="flex-auto w-64 h-screen flex items-center justify-center relative">
+        <img
+          src={backgroundImageUrl}
+          className="w-full h-full object-cover absolute md:hidden"
+          alt="Background"
+          style={imageStyle}
+        />
+        <Space direction="vertical" size={16}>
+          <Card
+            title={"Sign Up"}
+            className="md:w-[450px] w-[340px] mx-auto"
+            style={cardStyle}
+          >
+            <div className="grid grid-cols-1 gap-4">
+              <>
+                <div className="text-left flex flex-col gap-2">
+                  <label htmlFor="email">Email</label>
+                  <Input
+                    id="email"
+                    placeholder="Enter your email"
+                    className="bg-transparent"
+                  />
+                </div>
+              </>
+
+              <div className="text-left flex flex-col gap-2">
+                <label htmlFor="username">Username</label>
+                <Input
+                  id="username"
+                  placeholder="Enter your username"
+                  prefix={<UserOutlined className="site-form-item-icon" />}
+                  className="bg-transparent"
+                />
+              </div>
+              <div className="text-left flex flex-col gap-2">
+                <label htmlFor="password">Password</label>
+                <Input.Password
+                  id="password"
+                  placeholder="Input password"
+                  iconRender={(visible) =>
+                    visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
+                  }
+                  className="bg-transparent"
+                />
+              </div>
+              <div>
+                Already a user?{" "}
+                <Link href={"/sign-in"}>Log into your account</Link>
+              </div>
+              <button
+                className="p-2 bg-gray-800 text-white hover:rounded-full transition duration-300"
+                onClick={""}
+              >
+                Sign Up
+              </button>
+            </div>
+          </Card>
+        </Space>
+      </div>
     </div>
   );
 };
